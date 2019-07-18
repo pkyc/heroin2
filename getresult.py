9b6d4584d67c8521f84346be16cbec3548f8132f
peter <peter@MBP01341.local>
2019-07-18 14:02:29 +0800

    v3

--- getresult.py
+++ getresult.py
@@ -1,8 +1,7 @@
 #!/usr/local/bin/pythonw
 
 import urllib2
-import re
-import string
+import sys
 import json
 from pymongo import MongoClient
 
@@ -10,16 +9,23 @@ client = MongoClient('localhost', 27017)
 fb = client.fb   
 odds = fb.odds
 
-link = 'https://bet.hkjc.com/football/getJSON.aspx?jsontype=results.aspx'
+def connection(tries=0):
+	link = 'https://bet.hkjc.com/football/getJSON.aspx?jsontype=results.aspx'
+	try:
+		htmltext = urllib2.urlopen(link).read()
+		y = len(json.loads(htmltext))
+		print ("connection success", y)
+		return(htmltext)
+	except Exception:
+		if tries < sys.getrecursionlimit():
+				return connection(tries+1)
+		else:
+			print ("finally connection failed")
+			exit()
 
-try:
-	htmltext = urllib2.urlopen(link).read()
-	y = len(json.loads(htmltext))
-	print ("connection success", y)
-except:
-	print ("connection failed")
-	exit()
 
+
+htmltext = connection()
 am = json.loads(htmltext)[1]
 z = len(am["matches"])
 
@@ -29,16 +35,15 @@ for i in range (0, z):
 	h = am["matches"][i]["accumulatedscore"][1]["home"]	
 	a = am["matches"][i]["accumulatedscore"][1]["away"]
 	if (h>a):
-		whowin = h
-	elseif (a>h):
-		whowin = a
-	else
-		whowin =d
+		whowin = "H"
+	elif (a>h):
+		whowin = "A"
+	else:
+		whowin = "D"
 	print h,a,whowin
-
-
-	result = fb.odds.update({"matchIDinofficial": matchIDinofficial}, {"$set": {"win": win, "win_odd": [win].odd "flag": 'odd'}}, upsert=True )
-	print D
+	winodd = fb.odds.find_one({"matchIDinofficial" : matchIDinofficial},{"_id": 0, whowin: 1})[whowin]
+	result = fb.odds.update({"matchIDinofficial": matchIDinofficial}, {"$set": {"win": whowin, "win_odd": winodd "flag": 'result'}}, upsert=True )
+	#print D
 
 
 # database = fb
